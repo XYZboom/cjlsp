dbbd23fe84713c10e3840bb36674a59a395ad8ec
cj-rust <cj-rust@local>
2026-08-28 17:03:33 +0800

    chore: cleanup T30 merge

--- commit_t30.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd /root/Code/cangjie/cj-lang/.worktrees/t_48e3d2e7
-git add -A
-git -c user.name="cj-rust" -c user.email="cj-rust@local" commit -m "T30: LLT success improvements (wildcard/token handling)
-
-- token/parser fixes per LLT failure clusters (wildcard, identifier-likes)
-- workspace 100 tests, clippy -D warnings clean" 2>&1 | tail -1
-git log --oneline -1
